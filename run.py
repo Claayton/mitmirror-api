65bc152e84fce186dcaf8bf760508d0f46819db4
Clayton Garcia <claaytongarcia@gmail.com>
2021-10-03 01:12:13 -0300

    config: deploy dificil

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 
 
 manager = Manager(create_app)
-manager.add_command('db', MigrateCommand)
+# manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
