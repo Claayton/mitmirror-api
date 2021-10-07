f7d95e65f9cee2d906bfae02120afcb2041e8fec
Clayton Garcia <claaytongarcia@gmail.com>
2021-10-07 13:33:03 -0300

    feature: Adicionada pagina admin para administrar dados do app

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ from app.extensions import authentication
 from app.extensions import encryptation
 from app.extensions import route_crossing
 from app.extensions import migrations
+from app.extensions import admin
 
 from app.routes.users.users_routes import users_routes_bp
-from app.routes.auth.auth_routes import auth_routes_bp
+from app.routes.auth.auth_routes import auth_routes_bp, authenticate
 from app.routes.index.index_routes import index_routes_bp
 
 def minimal_app(config_file):
@@ -19,10 +20,11 @@ def create_app():
     app = minimal_app(config_file='config')
 
     database.init_app(app)
-    authentication.init_app(app)
+    # authentication.init_app(app)
     encryptation.init_app(app)
     route_crossing.init_app(app)
     migrations.init_app(app)
+    admin.init_app(app)
     
     app.register_blueprint(users_routes_bp)
     app.register_blueprint(auth_routes_bp)
@@ -37,6 +39,7 @@ def tests_app():
     encryptation.init_app(app)
     route_crossing.init_app(app)
     migrations.init_app(app)
+    admin.init_app(app)
     
     app.register_blueprint(users_routes_bp)
     app.register_blueprint(auth_routes_bp)
