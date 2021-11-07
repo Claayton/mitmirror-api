a61fd5f8795e8d2986be92f2ce0dc8ed4dba6348
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-07 02:00:38 -0300

    update: Adicionado decorators de cross e login required para alguns endpoints

--- app/routes/users/users_routes.py
+++ app/routes/users/users_routes.py
@@ -1,25 +1,33 @@
 from app.routes.users import users_ctrl
 from flask import Blueprint
+from flask_cors import cross_origin
+from flask_login import login_required
 
 bp = Blueprint("users_routes_bp", __name__)
-
 
 @bp.route('/api/users/', methods=['GET'])
 def get_users():
     return users_ctrl.get_users()
 
+@cross_origin()
+@login_required
 @bp.route('/api/users/<id>/', methods=['GET'])
 def get_user(id):
     return users_ctrl.get_user(id)
 
+@cross_origin()
 @bp.route('/api/users/', methods=['POST'])
 def post_user():
     return users_ctrl.post_user()
 
+@cross_origin()
+@login_required
 @bp.route('/api/users/<id>', methods=['PUT'])
 def put_user(id):
     return users_ctrl.update_user(id)
 
+@cross_origin()
+@login_required
 @bp.route('/api/users/<id>', methods=['DELETE'])
 def delete_user(id):
     return users_ctrl.delete_user(id)
