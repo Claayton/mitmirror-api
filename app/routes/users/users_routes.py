768fc8bcf50beb9196c9245b17b4f276567a8e0b
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-07 02:35:37 -0300

    bugfixes: Alterado as ordens dos decorators para users e alterados status codes dos testes das mesmas

--- app/routes/users/users_routes.py
+++ app/routes/users/users_routes.py
@@ -9,25 +9,25 @@ bp = Blueprint("users_routes_bp", __name__)
 def get_users():
     return users_ctrl.get_users()
 
+@bp.route('/api/users/<id>/', methods=['GET'])
 @cross_origin()
 @login_required
-@bp.route('/api/users/<id>/', methods=['GET'])
 def get_user(id):
     return users_ctrl.get_user(id)
 
-@cross_origin()
 @bp.route('/api/users/', methods=['POST'])
+@cross_origin()
 def post_user():
     return users_ctrl.post_user()
 
+@bp.route('/api/users/<id>/', methods=['PUT'])
 @cross_origin()
 @login_required
-@bp.route('/api/users/<id>', methods=['PUT'])
 def put_user(id):
     return users_ctrl.update_user(id)
 
+@bp.route('/api/users/<id>/', methods=['DELETE'])
 @cross_origin()
 @login_required
-@bp.route('/api/users/<id>', methods=['DELETE'])
 def delete_user(id):
     return users_ctrl.delete_user(id)
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -4,7 +4,7 @@ def test_if_the_index_route_is_working(client):
     assert response.status_code == 200
 
 def test_if_the_root_route_is_working(client):
-    url = "/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJleHAiOjE2MzM1NDA3Mjl9.laQk29KJqGv1ojVEK5-Tgig0xCWfVCoQBNaS10cJI1Y"
+    url = "/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzYyNzcwNjAsImlhdCI6MTYzNjI2MjY2MCwic3ViIjoxLCJuYW1lIjoiVGVzdCJ9.ZdqQyLHX0MyhPyYkWJdnWAr7dSWtX7UlHESNeK4IFow"
     response = client.get(url)
     assert response.status_code == 200
 
@@ -20,7 +20,8 @@ def test_if_the_auth_route_is_working(client):
 def test_if_the_get_user_route_is_working(client):
     url = "/api/users/1/"
     response = client.get(url)
-    assert response.status_code == 200
+    print(response.data)
+    assert response.status_code == 401
 
 def test_if_the_get_users_route_is_working(client):
     url = "/api/users/"
@@ -39,7 +40,7 @@ def test_if_the_post_users_route_is_working(client):
     assert response.status_code == 403
 
 def test_if_the_put_users_route_is_working(client):
-    url ="/api/users/1"
+    url ="/api/users/1/"
     data = {
         "name": "Test",
         "email": "test@test.com",
@@ -47,9 +48,9 @@ def test_if_the_put_users_route_is_working(client):
         "password": "test123"
     }
     response = client.put(url, json=data)
-    assert response.status_code == 201
+    assert response.status_code == 401
 
 def test_if_the_delete_users_route_is_working(client):
-    url = "/api/users/666"
+    url = "/api/users/666/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJleHAiOjE2MzM1NDA3Mjl9.laQk29KJqGv1ojVEK5-Tgig0xCWfVCoQBNaS10cJI1Y"
     response = client.delete(url)
-    assert response.status_code == 403
+    assert response.status_code == 401
