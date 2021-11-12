4c103fa515873b2d9048770c5dc7531a1125c77a
Clayton Garcia <claaytongarcia@gmail.com>
2021-11-12 12:43:32 -0300

    update: Adicionado ulr_prefix para as rotas users

--- app/routes/users/users_routes.py
+++ app/routes/users/users_routes.py
@@ -3,30 +3,30 @@ from flask import Blueprint
 from flask_cors import cross_origin
 from flask_login import login_required
 
-bp = Blueprint("users_routes_bp", __name__)
+bp = Blueprint("users_routes_bp", __name__, url_prefix='/api')
 
-@bp.route('/api/users/', methods=['GET'])
+@bp.route('/users/', methods=['GET'])
 def get_users():
     return users_ctrl.get_users()
 
-@bp.route('/api/users/<id>/', methods=['GET'])
+@bp.route('/users/<id>/', methods=['GET'])
 @login_required
 @cross_origin()
 def get_user(id):
     return users_ctrl.get_user(id)
 
-@bp.route('/api/users/', methods=['POST'])
+@bp.route('/users/', methods=['POST'])
 @cross_origin()
 def post_user():
     return users_ctrl.post_user()
 
-@bp.route('/api/users/<id>/', methods=['PUT'])
+@bp.route('/users/<id>/', methods=['PUT'])
 @login_required
 @cross_origin()
 def put_user(id):
     return users_ctrl.update_user(id)
 
-@bp.route('/api/users/<id>/', methods=['DELETE'])
+@bp.route('/users/<id>/', methods=['DELETE'])
 @login_required
 @cross_origin()
 def delete_user(id):
