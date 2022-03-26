0631acdf4cdf7df5dd6447f88d57ca2d1041a5b2
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-26 17:13:19 -0300

    refactor: Alterado a posicao do metodo insert_user da classe UserRepository e alterado alguns parametros que estavam escritos errado

--- mitmirror/infra/repository/users_repository.py
+++ mitmirror/infra/repository/users_repository.py
@@ -16,6 +16,73 @@ class UserRepository(UserRepositoryInterface):
 
         self.__connection_string = connection_string
 
+    def insert_user(
+        self,
+        name: str,
+        email: str,
+        username: str,
+        password_hash: str,
+        secundary_id: int = 0,  # Configurar futuramente
+        is_staff: bool = False,  # Configurar futuramente
+        is_active_user: bool = False,  # Configurar futuramente
+        date_joined: Type[datetime] = datetime.today(),  # Configurar futuramente
+        last_login: Type[datetime] = datetime.today(),
+    ) -> User:
+        """
+        Realiza a inserçao de um novo usuario na tabela User
+        :param name: Nome do usuario.
+        :param email: Email do usuario.
+        :param username: Nome de usuario que deve ser unico para cada um.
+        :param password_hash: Hash da senha do usuario.
+        :param secundary_id: Numero secundario de identificacao do usuario.
+        :param is_staff: Se o usuario e ou nao o admin sistema.
+        :param is_active_user: Se o usuario e esta ativo no sistema.
+        :param date_joined: Data de cadastro do usuario no sistema.
+        :param last_login: Data do ultimo login do usuario no sistema.
+        :return: O usuario cadastrado e seus dados.
+        """
+
+        with DataBaseConnectionHandler(self.__connection_string) as database:
+
+            try:
+
+                new_user = UserModel(
+                    name=name,
+                    email=email,
+                    username=username,
+                    password_hash=password_hash,
+                    secundary_id=secundary_id,
+                    is_staff=is_staff,
+                    is_active_user=is_active_user,
+                    date_joined=date_joined,
+                    last_login=last_login,
+                )
+                database.session.add(new_user)
+                database.session.commit()
+
+                return User(
+                    id=new_user.id,
+                    name=new_user.name,
+                    email=new_user.email,
+                    username=new_user.username,
+                    password_hash=new_user.password_hash,
+                    secundary_id=new_user.secundary_id,
+                    is_staff=new_user.is_staff,
+                    is_active_user=new_user.is_active_user,
+                    date_joined=new_user.date_joined,
+                    last_login=new_user.last_login,
+                )
+
+            except Exception as error:
+
+                database.session.rollback()
+                # raise DefaultError(message=str(error)) from error
+                raise error
+
+            finally:
+
+                database.session.close()
+
     def get_user(
         self, user_id: int = None, username: str = None, email: str = None
     ) -> User:
@@ -107,72 +174,6 @@ class UserRepository(UserRepositoryInterface):
 
             database.session.close()
 
-    def insert_user(
-        self,
-        name: str,
-        email: str,
-        username: str,
-        password_hash: str,
-        secondary_id: int = 0,  # Configurar futuramente
-        is_staff: bool = False,  # Configurar futuramente
-        is_active: bool = False,  # Configurar futuramente
-        date_joined: Type[datetime] = None,
-        last_login: Type[datetime] = datetime.today(),
-    ) -> User:
-        """
-        Realiza a inserçao de um novo usuario na tabela User
-        :param name: Nome do usuario.
-        :param email: Email do usuario.
-        :param username: Nome de usuario que deve ser unico para cada um.
-        :param password_hash: Hash da senha do usuario.
-        :param secondary_id: Numero secundario de identificacao do usuario.
-        :param is_staff: Se o usuario e ou nao o admin sistema.
-        :param is_active: Se o usuario e esta ativo no sistema.
-        :param date_joined: Data de cadastro do usuario no sistema.
-        :param kast_login: Data do ultimo login do usuario no sistema.
-        :return: O usuario cadastrado e seus dados.
-        """
-
-        with DataBaseConnectionHandler(self.__connection_string) as database:
-
-            try:
-
-                new_user = UserModel(
-                    name=name,
-                    email=email,
-                    username=username,
-                    password_hash=password_hash,
-                    secondary_id=secondary_id,
-                    is_staff=is_staff,
-                    is_active=is_active,
-                    date_joined=date_joined,
-                    last_login=last_login,
-                )
-                database.session.add(new_user)
-                database.session.commit()
-
-                return User(
-                    id=new_user.id,
-                    name=new_user.name,
-                    email=new_user.email,
-                    username=new_user.username,
-                    password_hash=new_user.password_hash,
-                    secondary_id=new_user.secondary_id,
-                    is_staff=new_user.is_staff,
-                    is_active=new_user.is_active,
-                    date_joined=new_user.date_joined,
-                    last_login=new_user.last_login,
-                )
-
-            except Exception as error:
-
-                database.session.rollback()
-                raise DefaultError(message=str(error)) from error
-
-            finally:
-
-                database.session.close()
-
     def update_user(
         self,
         user_id: int,
@@ -180,9 +181,9 @@ class UserRepository(UserRepositoryInterface):
         email: str = None,
         username: str = None,
         password_hash: str = None,
-        secondary_id: int = None,
+        secundary_id: int = None,
         is_staff: bool = None,
-        is_active: bool = None,
+        is_active_user: bool = None,
         date_joined: Type[datetime] = None,
         last_login: Type[datetime] = None,
     ) -> User:
@@ -193,9 +194,9 @@ class UserRepository(UserRepositoryInterface):
         :param email: Email do usuario.
         :param username: Nome de usuario que deve ser unico para cada um.
         :param password_hash: Hash da senha do usuario.
-        :param secondary_id: Numero secundario de identificacao do usuario.
+        :param secundary_id: Numero secundario de identificacao do usuario.
         :param is_staff: Se o usuario e ou nao o admin sistema.
-        :param is_active: Se o usuario e esta ativo no sistema.
+        :param is_active_user: Se o usuario e esta ativo no sistema.
         :param date_joined: Data de cadastro do usuario no sistema.
         :param kast_login: Data do ultimo login do usuario no sistema.
         :return: O usuario com seus dados atualizados.
@@ -245,9 +246,9 @@ class UserRepository(UserRepositoryInterface):
                 if username:
                     user.username = username
                 user.password_hash = password_hash
-                user.secondary_id = secondary_id
+                user.secundary_id = secundary_id
                 user.is_staff = is_staff
-                user.is_active = is_active
+                user.is_active_user = is_active_user
                 user.date_joined = date_joined
                 user.last_login = last_login
 
@@ -259,9 +260,9 @@ class UserRepository(UserRepositoryInterface):
                     email=user.email,
                     username=user.username,
                     password_hash=user.password_hash,
-                    secondary_id=user.secondary_id,
+                    secundary_id=user.secundary_id,
                     is_staff=user.is_staff,
-                    is_active=user.is_active,
+                    is_active_user=user.is_active_user,
                     date_joined=user.date_joined,
                     last_login=user.last_login,
                 )
