fb457087b03dd6857c6d889b731ad8d06fa25a0a
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-26 17:17:37 -0300

    refactor: Realocados arquivos de teste

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from mitmirror import tests_app
-
-@pytest.fixture(scope="module")
-def app():
-    """Instance of Main flask-app"""
-    return tests_app()
--- tests/testes_routes.py
+++ tests/testes_routes.py
@@ -3,51 +3,57 @@ def test_if_the_index_route_is_working(client):
     response = client.get(url)
     assert response.status_code == 200
 
+
 def test_if_the_root_route_is_working(client):
-    url = "/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJleHAiOjE2MzM1NDA3Mjl9.laQk29KJqGv1ojVEK5-Tgig0xCWfVCoQBNaS10cJI1Y"
+    url = "/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.\
+        eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJleHAiOjE2MzM1NDA3Mjl9.\
+        laQk29KJqGv1ojVEK5-Tgig0xCWfVCoQBNaS10cJI1Y"
     response = client.get(url)
     assert response.status_code == 200
 
+
 def test_if_the_auth_route_is_working(client):
-    url ="/api/auth/"
-    data = {
-        "username": "test123",
-        "password": "test123"
-    }
+    url = "/api/auth/"
+    data = {"username": "test123", "password": "test123"}
     response = client.post(url, json=data)
     assert response.status_code == 200
+
 
 def test_if_the_get_user_route_is_working(client):
     url = "/api/users/1/"
     response = client.get(url)
     assert response.status_code == 200
 
+
 def test_if_the_get_users_route_is_working(client):
     url = "/api/users/"
-    response =client.get(url)
-    assert response.json["data"]["1"]["name"] == 'Test'
+    response = client.get(url)
+    assert response.json["data"]["1"]["name"] == "Test"
+
 
 def test_if_the_post_users_route_is_working(client):
-    url ="/api/users/"
+    url = "/api/users/"
     data = {
         "name": "Test",
         "email": "test@test.com",
         "username": "test123",
-        "password": "test123"
+        "password": "test123",
     }
     response = client.post(url, json=data)
     assert response.status_code == 403
 
+
 def test_if_the_put_users_route_is_working(client):
-    url ="/api/users/1"
+    url = "/api/users/1"
     data = {
         "name": "Test",
         "email": "test@test.com",
         "username": "test123",
-        "password": "test123"
+        "password": "test123",
     }
     response = client.put(url, json=data)
     assert response.status_code == 201
+
 
 def test_if_the_delete_users_route_is_working(client):
     url = "/api/users/666"
