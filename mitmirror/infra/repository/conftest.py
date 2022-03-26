499283e5cfc69185a4a9c94bf555d020eba2ed42
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-26 20:15:49 -0300

    test:recycle:: Implementado a fixture mitmirror/infra/repository/conftest.py

--- mitmirror/infra/repository/conftest.py
+++ mitmirror/infra/repository/conftest.py
@@ -50,7 +50,7 @@ def user_repository_with_delete_user(
 
 
 @fixture
-def user_repository_with_user_already_registered(
+def user_repository_with_one_user_registered(
     user_repository, mock_user
 ):  # pylint: disable=W0621
     """Fixture para montar o objeto UserRepository com usuario registrado"""
@@ -91,3 +91,77 @@ def user_repository_with_user_already_registered(
 
     engine = database.get_engine()
     engine.execute(f"DELETE FROM users WHERE username='{username}';")
+
+
+@fixture
+def user_repository_with_two_users_registered(
+    user_repository, mock_user
+):  # pylint: disable=W0621
+    """Fixture para montar o objeto UserRepository com usuarios registrados"""
+
+    engine = database.get_engine()
+    engine.execute(
+        f"""
+        INSERT INTO users (
+            id,
+            name,
+            email,
+            username,
+            password_hash,
+            secundary_id,
+            is_staff,
+            is_active_user,
+            last_login,
+            date_joined
+        )
+        VALUES (
+            '{mock_user["user_id"]}',
+            '{mock_user["name"]}',
+            '{mock_user["email"]}',
+            '{mock_user["username"]}',
+            '{mock_user["password_hash"]}',
+            '{mock_user["secundary_id"]}',
+            '{mock_user["is_staff"]}',
+            '{mock_user["is_active_user"]}',
+            '{mock_user["last_login"]}',
+            '{mock_user["date_joined"]}'
+        );
+        """
+    )
+    engine.execute(
+        f"""
+        INSERT INTO users (
+            id,
+            name,
+            email,
+            username,
+            password_hash,
+            secundary_id,
+            is_staff,
+            is_active_user,
+            last_login,
+            date_joined
+        )
+        VALUES (
+            '{mock_user["user_id"]}2',
+            '{mock_user["name"]}2',
+            '{mock_user["email"]}2',
+            '{mock_user["username"]}2',
+            '{mock_user["password_hash"]}2',
+            '{mock_user["secundary_id"]}2',
+            '{mock_user["is_staff"]}2',
+            '{mock_user["is_active_user"]}2',
+            '{mock_user["last_login"]}2',
+            '{mock_user["date_joined"]}2'
+        );
+        """
+    )
+
+    username1 = mock_user["username"]
+    username2 = f'{mock_user["username"]}2'
+
+    yield user_repository
+
+    engine = database.get_engine()
+    engine.execute(f"DELETE FROM users WHERE username='{username1}';")
+    engine.execute(f"DELETE FROM users WHERE username='{username2}';")
