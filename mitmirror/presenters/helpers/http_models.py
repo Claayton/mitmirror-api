7e201f58cc14775b5d1d83cc49b2602593c874aa
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-02 15:54:39 -0300

    feat✨: Implementado helpers de HttpRequest e HttpResponse para os controllers

---
+++ mitmirror/presenters/helpers/http_models.py
@@ -0,0 +1,29 @@
+"""Arquivo para Http Models"""
+from typing import Dict
+
+
+class HttpRequest:
+    """Representaçao de requisicoes"""
+
+    def __init__(
+        self, header: Dict = None, body: Dict = None, query: Dict = None
+    ) -> None:
+        self.header = header
+        self.body = body
+        self.query = query
+
+    def __repr__(self):
+        return (
+            f"HttpRequest (header={self.header}, body={self.body}, query={self.query})"
+        )
+
+
+class HttpResponse:
+    """Representaçao de respostas"""
+
+    def __init__(self, status_code: int, body: Dict) -> None:
+        self.status_code = status_code
+        self.body = body
+
+    def __repr__(self):
+        return f"HttpResponse (status_code={self.status_code}, body={self.body})"
