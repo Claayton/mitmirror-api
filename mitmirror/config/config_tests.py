8cce2166bd4b2c2b1c1555eb4d2d601c6ed81cca
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-02 15:30:44 -0300

    refactor🗑️: Removido arquivo de configuracoes de testes que nao e mais util

--- mitmirror/config/config_tests.py
+++
@@ -1,8 +0,0 @@
-"""Configuracoes de testes"""
-from .database import database_infos
-
-JSONIFY_PRETTYPRINT_REGULAR = True
-
-SECRET_KEY = database_infos["secret_key"]
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///tests.sqlite3"
