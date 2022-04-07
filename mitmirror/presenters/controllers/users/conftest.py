654452dce16da4c0ec501db203d032b36eb86ae0
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-07 16:59:20 -0300

    test✅: Atualizados os testes para GetUserController

--- mitmirror/presenters/controllers/users/conftest.py
+++ mitmirror/presenters/controllers/users/conftest.py
@@ -1,9 +1,10 @@
 """Arquivo para fixtures"""
 from pytest import fixture
-from mitmirror.infra.tests import mock_user
-from mitmirror.data.users import GetUser
-from mitmirror.infra.tests import UserRepositorySpy
-from .get_user_controller import GetUserController
+from mitmirror.infra.repository import UserRepository
+from mitmirror.infra.tests import UserRepositorySpy, mock_user
+from mitmirror.data.users import GetUser, GetUsers
+from mitmirror.config import CONNECTION_STRING_TEST
+from . import GetUserController, GetUsersController
 
 
 user = mock_user()
@@ -20,6 +21,13 @@ def fake_user():
 def user_repository():
     """Montagem de user_repository"""
 
+    return UserRepository(CONNECTION_STRING_TEST)
+
+
+@fixture
+def user_repository_spy():
+    """Montagem de user_repository_spy"""
+
     return UserRepositorySpy()
 
 
@@ -28,6 +36,16 @@ def get_user_controller(user_repository):  # pylint: disable=W0621
     """Montagem de get_user_controller"""
 
     usecase = GetUser(user_repository)
+    controller = GetUserController(usecase)
+
+    return controller
+
+
+@fixture
+def get_user_controller_with_spy(user_repository_spy):  # pylint: disable=W0621
+    """Montagem de get_user_controller utilizando o spy"""
+
+    usecase = GetUser(user_repository_spy)
     controller = GetUserController(usecase)
 
     return controller
--- mitmirror/presenters/controllers/users/get_user_controller_test.py
+++ mitmirror/presenters/controllers/users/get_user_controller_test.py
@@ -1,57 +1,71 @@
 """Testes para GetUserController"""
+from pytest import raises
+from mitmirror.errors import HttpNotFound, HttpBadRequestError, HttpUnprocessableEntity
 from mitmirror.infra.tests import mock_user
-from mitmirror.presenters.helpers import HttpRequest
 
 
 user = mock_user()
 
 
-def test_handler_with_query_params_user_id(
-    get_user_controller, user_repository, fake_user
-):
+def test_handler(get_user_controller_with_spy, user_repository_spy, fake_user):
     """Testando o metodo handler, buscando pelo id do usuario"""
 
-    attributes = {"user_id": fake_user.id}
+    param = fake_user.id
 
-    response = get_user_controller.handler(HttpRequest(query=attributes))
+    response = get_user_controller_with_spy.handler(param=param)
 
     # Testando as entradas:
-    assert user_repository.get_user_params["user_id"] == attributes["user_id"]
+    assert user_repository_spy.get_user_params["user_id"] == param
 
     # Testando as saidas:
     assert response.status_code == 200
     assert "error" not in response.body
 
 
-def test_handler_with_query_param_email(
-    get_user_controller, user_repository, fake_user
-):
-    """Testando o metodo handler, buscando pelo email do usuario"""
+def test_handler_error_with_invalid_user_id(get_user_controller_with_spy, fake_user):
+    """
+    Testando o erro no metodo handler.
+    Onde e passado um valor invalido para o parametro user_id.
+    Deve retornar um erro HttpUnprocessableEntity.
+    """
 
-    attributes = {"email": fake_user.email}
+    with raises(HttpUnprocessableEntity) as error:
 
-    response = get_user_controller.handler(HttpRequest(query=attributes))
+        param = fake_user.name
 
-    # Testando as entradas:
-    assert user_repository.get_user_params["email"] == attributes["email"]
-
-    # Testando as saidas:
-    assert response.status_code == 200
-    assert "error" not in response.body
-
-
-def test_handler_with_query_param_username(
-    get_user_controller, user_repository, fake_user
-):
-    """Testando o metodo handler, buscando pelo email do email"""
-
-    attributes = {"username": fake_user.username}
-
-    response = get_user_controller.handler(HttpRequest(query=attributes))
-
-    # Testando as entradas:
-    assert user_repository.get_user_params["username"] == attributes["username"]
+        get_user_controller_with_spy.handler(param=param)
 
     # Testando as saidas:
-    assert response.status_code == 200
-    assert "error" not in response.body
+    assert "error" in str(error.value)
+
+
+def test_handler_error_without_user_id_param(get_user_controller_with_spy):
+    """
+    Testando o erro no metodo handler.
+    Onde nao e passado o parametro de user_id.
+    Deve retornar um erro HttpBadRequestError.
+    """
+
+    with raises(HttpBadRequestError) as error:
+
+        get_user_controller_with_spy.handler()
+
+    # Testando as saidas:
+    assert "error" in str(error.value)
+
+
+def test_handler_error_not_found(get_user_controller, fake_user):
+    """
+    Testando o erro no metodo handler.
+    Onde nenhum usuario e encontrado no banco de dados.
+    Deve retornar um erro HttpNotFound.
+    """
+
+    with raises(HttpNotFound) as error:
+
+        param = fake_user.id
+
+        get_user_controller.handler(param=param)
+
+    # Testando as saidas:
+    assert "error" in str(error.value)
