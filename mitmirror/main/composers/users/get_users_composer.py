b240fdad7ca79ff127df2157c4c79d8a1fd93cb0
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-08 20:20:08 -0300

    fix🐛: Alterado connection_string_test e a classe GetUsers, que estavam erradas

--- mitmirror/main/composers/users/get_users_composer.py
+++ mitmirror/main/composers/users/get_users_composer.py
@@ -2,17 +2,17 @@
 from typing import Type
 from mitmirror.infra.repository import UserRepository
 from mitmirror.data.interfaces import UserRepositoryInterface
-from mitmirror.data.users import GetUser
+from mitmirror.data.users import GetUsers
 from mitmirror.presenters.controllers.users import GetUsersController
-from mitmirror.config import CONNECTION_STRING
+from mitmirror.config import CONNECTION_STRING_TEST
 
 
 def get_users_composer(
-    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING),
+    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING_TEST),
 ):
     """Montagem do caso de uso GetUsers"""
 
-    usecase = GetUser(infra)
+    usecase = GetUsers(infra)
     controller = GetUsersController(usecase)
 
     return controller
