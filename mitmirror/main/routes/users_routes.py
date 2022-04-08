bff1f95a087723aba87d3560270f0478f9defedc
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-08 20:49:34 -0300

    feat✨: Implementado a rota get_user + Tests

--- mitmirror/main/routes/users_routes.py
+++ mitmirror/main/routes/users_routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request as RequestFastApi
 from fastapi.responses import JSONResponse
 from mitmirror.main.adapters import request_adapter
 from mitmirror.presenters.errors import handler_errors
-from mitmirror.main.composers.users import get_users_composer
+from mitmirror.main.composers.users import get_users_composer, get_user_composer
 
 users = APIRouter(prefix="/api/users", tags=["user"])
 
@@ -19,6 +19,28 @@ async def get_users(request: RequestFastApi):
         response = None
         controller = get_users_composer()
         response = await request_adapter(request, controller.handler)
+
+    except Exception as error:  # pylint: disable=W0703
+
+        response = handler_errors(error)
+
+    return JSONResponse(status_code=response.status_code, content=response.body)
+
+
+@users.get("/{user_id}/")
+async def get_user(request: RequestFastApi, user_id: int):
+    """
+    Rota para buscar um usuario registrado no sistema.
+    :param user_id: ID do usuario para busca no db.
+    :return: Um usuario e informacoes do mesmo.
+    """
+    print(user_id)
+    response = None
+
+    try:
+
+        controller = get_user_composer()
+        response = await request_adapter(request, controller.handler, user_id)
 
     except Exception as error:  # pylint: disable=W0703
 
--- mitmirror/main/routes/users_routes_test.py
+++ mitmirror/main/routes/users_routes_test.py
@@ -13,3 +13,17 @@ def test_get_users(client_with_one_user):
     assert isinstance(response.json()["data"], list)
     assert "id" in response.json()["data"][0]
     assert "name" in response.json()["data"][0]
+
+
+def test_get_user(client_with_one_user, fake_user):
+    """Testando a rota get_user"""
+
+    url = f"/api/users/{fake_user.id}"
+
+    response = client_with_one_user.get(url=url)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], dict)
+    assert "id" in response.json()["data"]
+    assert "name" in response.json()["data"]
