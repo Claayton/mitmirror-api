74ce07fbc7b7b0be3c2075fa2fdd53c8139f14dc
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-08 22:07:37 -0300

    fix🐛: Alterado connection_string_test que estava errada

--- mitmirror/main/composers/users/delete_user_composer.py
+++ mitmirror/main/composers/users/delete_user_composer.py
@@ -4,11 +4,11 @@ from mitmirror.infra.repository import UserRepository
 from mitmirror.data.interfaces import UserRepositoryInterface
 from mitmirror.data.users import DeleteUser
 from mitmirror.presenters.controllers.users import DeleteUserController
-from mitmirror.config import CONNECTION_STRING
+from mitmirror.config import CONNECTION_STRING_TEST
 
 
 def delete_user_composer(
-    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING),
+    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING_TEST),
 ):
     """Montagem do caso de uso DeleteUser"""
 
