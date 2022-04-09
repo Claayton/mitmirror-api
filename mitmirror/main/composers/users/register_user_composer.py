aa636bdcdfe27431e4eeeaf5888ea41d9dbf20bc
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-08 21:08:15 -0300

    fix🐛: Alterado connection_string_test que estava errada

--- mitmirror/main/composers/users/register_user_composer.py
+++ mitmirror/main/composers/users/register_user_composer.py
@@ -5,11 +5,11 @@ from mitmirror.data.interfaces import UserRepositoryInterface
 from mitmirror.data.users import RegisterUser
 from mitmirror.data.security import PasswordHash
 from mitmirror.presenters.controllers.users import RegisterUserController
-from mitmirror.config import CONNECTION_STRING
+from mitmirror.config import CONNECTION_STRING_TEST
 
 
 def register_user_composer(
-    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING),
+    infra: Type[UserRepositoryInterface] = UserRepository(CONNECTION_STRING_TEST),
 ):
     """Montagem do caso de uso RegisterUser"""
 
