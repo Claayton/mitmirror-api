741172f9b5a58b4857d90ee7817e5a6b7ce118fb
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-08 22:11:44 -0300

    refactor⚰️: Removido arquivos de codigos defasados que nao serao mais uteis para o projeto

--- mitmirror/routes/index/index_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from mitmirror.routes.index import index_ctrl
-
-bp = Blueprint('index_routes_bp', __name__)
-
-@bp.route('/')
-def index():
-    return jsonify({'message': 'Successfully fetched', 'routes': index_ctrl.index()}), 200
