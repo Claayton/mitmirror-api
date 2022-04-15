04167689959170b37bdcfe6b568ed247f0662c5c
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-15 18:49:42 -0300

    chore➕: Reorganizado as funcoes de criacao de bancos de dados para uma funcao apenas

--- mitmirror/infra/config/create_database.py
+++ mitmirror/infra/config/create_database.py
@@ -4,21 +4,16 @@ from mitmirror.infra.entities import *  # pylint: disable=W0401, W0614
 from mitmirror.infra.config import DataBaseConnectionHandler, Base
 
 
-def create_database_main():
-    """Criação do banco de dados de testes"""
+def create_databases():
+    """Criando bancos de dados"""
 
     db_connection = DataBaseConnectionHandler(CONNECTION_STRING)
-    engine = db_connection.get_engine()
-    base = Base.metadata.create_all(engine)
-
-    return base
-
-
-def create_database_tests():
-    """Criação do banco de dados de testes"""
-
     db_connection_tests = DataBaseConnectionHandler(CONNECTION_STRING_TEST)
+
+    engine = db_connection.get_engine()
     engine_tests = db_connection_tests.get_engine()
+
+    base = Base.metadata.create_all(engine)
     base = Base.metadata.create_all(engine_tests)
 
     return base
