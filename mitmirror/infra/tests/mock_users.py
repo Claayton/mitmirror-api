e6d876c8edd4206f441ad00a4dafdbd03cf6f985
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-20 21:16:46 -0300

    test🤡: Atualizado o mock_users, para entregar tanto uma password limpa, quanto um hash da password

--- mitmirror/infra/tests/mock_users.py
+++ mitmirror/infra/tests/mock_users.py
@@ -1,20 +1,43 @@
 """Arquivo de mock de usuarios"""
+from collections import namedtuple
 from faker import Faker
-from mitmirror.domain.models import User
+from mitmirror.data.security import PasswordHash
 
 
 fake = Faker()
+password_hash = PasswordHash()
+
+
+UserMock = namedtuple(
+    "User",
+    [
+        "id",
+        "name",
+        "email",
+        "username",
+        "password",
+        "password_hash",
+        "secundary_id",
+        "is_staff",
+        "is_active_user",
+        "last_login",
+        "date_joined",
+    ],
+)
 
 
 def mock_user():
     """Mock para users"""
 
-    return User(
+    password = fake.password()
+
+    return UserMock(
         id=fake.random_number(),
         name=fake.name(),
         email=fake.email(),
         username=fake.user_name(),
-        password_hash=fake.pystr(),
+        password=password,
+        password_hash=password_hash.hash(password).decode(),
         secundary_id=fake.random_number(),
         is_staff=False,
         is_active_user=False,
