5938f7b1148e34a16deebb41df06d9c97d62ecd0
Clayton Garcia <claaytongarcia@gmail.com>
2022-05-04 19:54:34 -0300

    test🤡: Implementado arquivo conftest com fixture de mock de banco de dados, criando um novo db para cada teste

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+"""Arquivo para fixtures"""
+from unittest.mock import patch
+from pytest import fixture
+from sqlmodel import create_engine, SQLModel
+from mitmirror.infra.entities import *  # pylint: disable=W0614, W0401
+
+
+@fixture(autouse=True, scope="function")
+def separate_database(request):
+    """
+    Cria um mock do banco de dados para que cada teste use um banco separado.
+    """
+
+    tmpdir = request.getfixturevalue("tmpdir")
+    test_db = tmpdir.join("mitmirror.test.db")
+    engine = create_engine(f"sqlite:///{test_db}")
+    SQLModel.metadata.create_all(engine)
+    with patch("mitmirror.infra.config.database_config.engine", engine):
+        yield
